41333ab28f79d0424ac7f165cacdb2c3aa031ce1
FadwaFouad <fadoalqasem@gmail.com>
2022-12-25 12:09:15 +0200

    Friends list

---
+++ lib/data/friend_repository.dart
@@ -0,0 +1,20 @@
+import 'package:friends_list/data/friend_model.dart';
+
+class FriendsRepo {
+  List<Friend> friends = [
+    Friend("Sami Ahmed", "Sami@gmail.com", "images/avatar_man1.jpg", fav: true),
+    Friend("Rani Osma", "Rani99@gmail.com", "images/avatar_man2.png"),
+    Friend("Mohmed Ahmed", "Mohmed1999@gmail.com", "images/avatar_man3.jpg"),
+    Friend("Fadwa Fuad", "Fadwa88@gmail.com", "images/avatar_girl.jpg",
+        fav: true),
+    Friend("daood Abdalla", "daad@gmail.com", "images/avatar_man2.png"),
+    Friend("Mohmed Ahmed", "Mohmed1999@gmail.com", "images/avatar_man3.jpg"),
+    Friend("Sami Ahmed", "Sami@gmail.com", "images/avatar_man1.jpg", fav: true),
+    Friend("Rana Fuad", "Mohmed1999@gmail.com", "images/avatar_girl.jpg"),
+    Friend("Sami Ahmed", "Sami@gmail.com", "images/avatar_man1.jpg"),
+    Friend("Rani Osma", "Rani99@gmail.com", "images/avatar_man2.png"),
+    Friend("Mohmed Ahmed", "Mohmed1999@gmail.com", "images/avatar_man3.jpg"),
+    Friend("renad Fuad", "Mohmed1999@gmail.com", "images/avatar_girl.jpg"),
+    Friend("Rani Osma", "Rani99@gmail.com", "images/avatar_man2.png"),
+  ];
+}
